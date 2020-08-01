4c119f075e88a1fd0d8dc9108cb0b7bee9165c9e
Karan Chouhan <karan.chouhan@mountblue.tech>
2020-08-01 05:43:00 +0000

    Update random.js

--- random.js
+++ random.js
@@ -53,22 +53,27 @@ task2();
 //#Task 3: Fetch a random number and a random string simultaneously, concatenate their
 //and print the concatenated string
 
-async function task3(){
-  let randomNumber= await fetchRandomNumbers();
-  let randomString = await fetchRandomString();
-  console.log(randomNumber+randomString)
+function task3(){
+  let result="";
+  fetchRandomNumbers()
+    .then((randomNum)=>result+=randomNum);
+  fetchRandomString()
+    .then((randomStr)=>result+=randomStr)
+    .then(()=>console.log(result));
 }
 
 task3();
 
 //#Task 4: Fetch 10 random numbers simultaneously -> and print their sum.
 
-async function task4(){
+function task4(){
   let sum=0;
-  for(i=0;i<10;i++){
-    sum+=await fetchRandomNumbers();
+   for(i=0;i<10;i++){
+    fetchRandomNumbers().then((randomNum)=>sum+=randomNum);
   }
-  console.log("sum is",sum)
+  setTimeout(() => {
+    console.log(sum)
+  }, 7000)
 }
 
 task4();
